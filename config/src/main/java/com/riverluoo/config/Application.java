e43f1fe0f0ee6521690721dbd457850a14e71808
wangyangyang <769789170@qq.com>
2019-05-22 16:12:32 +0800

    add config

--- config/src/main/java/com/riverluoo/config/Application.java
+++ config/src/main/java/com/riverluoo/config/Application.java
@@ -1,8 +1,20 @@
 package com.riverluoo.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @auther: wangyang
  * @since: 2019-05-22 15:42
  */
+@SpringCloudApplication
+@EnableConfigServer
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+
 }
