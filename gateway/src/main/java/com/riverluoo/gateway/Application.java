925680f3ba7b1342e43d40d07100f130ea67f8eb
wangyangyang <769789170@qq.com>
2019-05-27 15:24:01 +0800

    Add client

---
+++ gateway/src/main/java/com/riverluoo/gateway/Application.java
@@ -0,0 +1,16 @@
+package com.riverluoo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @auther: wangyang
+ * @since: 2019-05-27 15:17
+ */
+@SpringCloudApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
